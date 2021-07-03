5461f649e853765ef2fbfff128a9455e11814307
dmtgk <glubokovdmitry@gmail.com>
2021-07-03 16:28:28 +0300

    Minor fixes

--- backend/main.py
+++ backend/main.py
@@ -25,11 +25,15 @@ app = FastAPI(
 
 @app.post("/uploadfile/")
 async def create_upload_file(file: UploadFile = File(...)):
-    bf = await file.read(file.spool_max_size)
-    with open('receive.png', 'wb') as f:
-        f.write(bf)
-    class_name = classifier.classify('receive.png')
-    return {'class_name': class_name}
+    f_name = file.filename.split('.')
+    content_type = file.content_type.split('/')
+    if 'png' in f_name or 'png' in content_type:
+        bf = await file.read(file.spool_max_size)
+        with open('receive.png', 'wb') as f:
+            f.write(bf)
+        class_name = classifier.classify('receive.png')
+        return {'class_name': class_name}
+    return {'class_name': 'Wrong file format!'}
 
 
 if __name__ == "__main__":
